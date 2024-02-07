a491d187d0d5fee813196e3d77312648a14de120
tanakorn44 <ohmmanaza2544@gmail.com>
2024-02-07 15:49:17 +0700

    no message

--- visitor/lib/pages/select_floor.dart
+++ visitor/lib/pages/select_floor.dart
@@ -147,7 +147,11 @@ class _KeypadState extends State<Keypad> {
               ).then((_) => _resetInactivityTimer());
             },
             // ใช้ _isButtonSelected เพื่อควบคุมการเปิดใช้งานของปุ่ม
+<<<<<<< Updated upstream
             child: const Text('ตกลง'), // 'OK' or 'Confirm' button
+=======
+            child: const Text('ตกลง'),
+>>>>>>> Stashed changes
           ),
         ),
       ],
